f8912eadaf8e6d2e099cc008db25a8c189a50ba9
Eoin H <UltimateHurl@gmail.com>
2012-10-24 21:38:43 +0100

    Revert "Revert "Now working with Tumblr v2 API""
    This reverts commit 04aefcb29a8cd7c501759d949532304d0cc4e520.

--- robotwrites.py
+++ robotwrites.py
@@ -1,35 +1,96 @@
-from tumblr import Api
-import robotsettings
-import random
-import sys
-import os
+import oauth2 as oauth
+import random,sys,os,urllib,urllib2,json,urlparse
 
-cwpath=robotsettings.cwpath
-BLOG=robotsettings.blog
-USER=robotsettings.user
-PASSWORD=robotsettings.password
-TAGS=robotsettings.tags
-STATE=robotsettings.state
-stopword = "\n" # Since we split on whitespace, this can never be a word
-stopsentence = (".", "!", "?",) # Cause a "new sentence" if found at the end of a word
-sentencesep	 = "\n" #String used to seperate sentences
+settings = {}
 
-def makePost(title,body):
-	global posttimes
-	global BLOG
-	global USER
-	global PASSWORD
-	global TAGS
-	global STATE
-	api = Api(BLOG,USER,PASSWORD)
-	try:
-		post = api.write_regular(title,body,tags=TAGS,state=STATE)
-		print "Published: ", post['url']
-	except:
-		print "Published okay, tumblr complained though!"	#tumblr for some reason complains
+stopword = "\n"
+stopsentence = (".", "!", "?",) #for counting sentences
+sentencesep	 = "\n"
 
+def loadSettings():
+	global settings
+	json_data=open('robotsettings.json')
+	settings = json.load(json_data)
+	
+def saveSettings():
+	global settings
+	fout = open('robotsettings.json','w')
+	fout.write(json.dumps(settings, sort_keys=True, indent=4))
+	fout.close()
 
-def genPost(book):
+def authorize():
+	global settings
+	#Adapted directly from https://github.com/simplegeo/python-oauth2 3 stage auth example
+	consumer_key = settings['consumer_key']
+	consumer_secret = settings['consumer_secret']
+	
+	request_token_url = 'http://www.tumblr.com/oauth/request_token'
+	access_token_url = 'http://www.tumblr.com/oauth/access_token'
+	authorize_url = 'http://www.tumblr.com/oauth/authorize'
+	
+	consumer = oauth.Consumer(consumer_key, consumer_secret)
+	client = oauth.Client(consumer)
+	
+	# Step 1: Get a request token. This is a temporary token that is used for 
+	# having the user authorize an access token and to sign the request to obtain 
+	# said access token.
+	
+	resp, content = client.request(request_token_url, "GET")
+	if resp['status'] != '200':
+	    raise Exception("Invalid response %s." % resp['status'])
+	
+	request_token = dict(urlparse.parse_qsl(content))
+	# Step 2: Redirect to the provider. Since this is a CLI script we do not 
+	# redirect. In a web application you would redirect the user to the URL
+	# below.
+	
+	print "Go to the following link in your browser:"
+	print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
+	print 
+	# After the user has granted access to you, the consumer, the provider will
+	# redirect you to whatever URL you have told them to redirect to. You can 
+	# usually define this in the oauth_callback argument as well.
+	accepted = 'n'
+	while accepted.lower() == 'n':
+	    accepted = raw_input('Have you authorized me? (y/n) ')
+	oauth_verifier = raw_input('What is the Oauth_Verifer parameter? ')
+	
+	# Step 3: Once the consumer has redirected the user back to the oauth_callback
+	# URL you can request the access token the user has approved. You use the 
+	# request token to sign this request. After this is done you throw away the
+	# request token and use the access token returned. You should store this 
+	# access token somewhere safe, like a database, for future use.
+	token = oauth.Token(request_token['oauth_token'],
+	    request_token['oauth_token_secret'])
+	token.set_verifier(oauth_verifier)
+	client = oauth.Client(consumer, token)
+	
+	resp, content = client.request(access_token_url, "POST")
+	access_token = dict(urlparse.parse_qsl(content))
+	
+	settings['oauth_token'] = access_token['oauth_token']
+	print access_token['oauth_token']
+	settings['oauth_token_secret'] = access_token['oauth_token_secret']
+	print access_token['oauth_token_secret']
+
+def queuePost(body,tags=None,title=None):
+	global settings
+	post_address = "http://api.tumblr.com/v2/blog/"+settings['blog']+"/post"
+	data = {"type":"text","state":settings['state'],"body" : body, "oauth_token":settings['oauth_token']}
+	if tags != None:
+		data['tags'] = str(tags)
+	if title != None:
+		data['title'] = str(title)
+	consumer = oauth.Consumer(settings['consumer_key'], settings['consumer_secret'])
+	token = oauth.Token(settings['oauth_token'], settings['oauth_token_secret'])
+	client = oauth.Client(consumer,token)
+	resp, content = client.request(post_address, "POST", urllib.urlencode(data))
+	js = json.loads(content)
+ 	if js['meta']['status'] != 201: #something wrong
+		print js['meta']['msg']
+		print js
+
+def generatePost(book):
 	global stopword
 	global stopsentence
 	global sentencesep
@@ -74,19 +135,20 @@ def genPost(book):
 		w1, w2 = w2, newword
 	return " ".join(post)
 
-def post():
-	global TAGS
-	global cwpath
-	txt_files = filter(lambda x: x.endswith('.txt'), os.listdir(cwpath+'/books'))
+def addPostToQueue():
+	bookpath = os.path.join(os.getcwd(),'books')
+	txt_files = filter(lambda x: x.endswith('.txt'), os.listdir(bookpath))
 	book = random.choice(txt_files)
-	TAGS = TAGS + ",inspired by " + book.replace('-','by')[:book.find(".")+1]
+	TAGS = settings['tags'] + ",inspired by " + book.replace('-','by')[:book.find(".")+1]
 	if(book.find("-") != -1):
 		TAGS = TAGS + "," + book[:book.find("-")-1]
 		TAGS = TAGS + "," + book[book.find("-")+2:book.find(".")]
-	print TAGS	
-	makePost("",genPost(cwpath + "/books/"+ book))
-	
-#each week, generate 15 posts
-for x in xrange(15):
-	post()
-	TAGS=robotsettings.tags
+	queuePost(generatePost(os.path.join(bookpath,book)), tags=TAGS)
+
+if __name__ == '__main__':
+	loadSettings()
+	if "oauth_token" not in settings.keys():
+		authorize()
+	for x in xrange(settings['posts_per_run']):
+		addPostToQueue()
+	saveSettings()
