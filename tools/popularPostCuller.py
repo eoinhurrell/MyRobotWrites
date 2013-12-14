0c110f0595aeec5374eee71e46318c0675d1eaca
Eoin Hurrell <ultimatehurl@gmail.com>
2013-12-14 21:05:17 +0000

    Rewrote backup creator, added popular post culler.
    Rewrote backup creator for Oauth, modified the backup reader to read
    new format.
    Also added a popular post culler, which sifts through the backup file
    and prints the most liked posts.

--- tools/popularPostCuller.py
+++ tools/popularPostCuller.py
@@ -1,15 +1,20 @@
 import json
+from operator import itemgetter
 #import robotsettings
 #backup = robotsettings.backup_name
 backup = 'robot.backup.log'
 
 count = 0
+selected = []
+
 for line in open(backup, 'r'):
     js = json.loads(line)
     #print js.keys()
     if int(js['note_count']) != 0:
         if 'body' in js.keys():
             count += 1
-            print js
-            #print js['body'].encode('utf-8')
+            selected.append(js)
+selected = sorted(selected, key=itemgetter('note_count'))
+fout = open('popular.json', 'w')
+json.dump(selected, fout, sort_keys=False, indent=4)
 print 'Count: ' + str(count)
--- tools/robotbackup.py
+++ tools/robotbackup.py
@@ -1,34 +1,77 @@
-import robotsettings
-import urllib2, json
+#!/usr/bin/python
 
-fout = open(robotsettings.backup_name,'w')
+import oauth2 as oauth
+import random,sys,os,urllib,urllib2,json,urlparse,getopt
 
-info_address = "http://api.tumblr.com/v2/blog/"+robotsettings.hostname+"/info?api_key=" + robotsettings.authkey
-posts_address = "http://api.tumblr.com/v2/blog/"+robotsettings.hostname+"/posts?api_key=" + robotsettings.authkey
-total_posts = 9999
-offset = 0 
+config_file = "../robotsettings.json"
+settings = {}
 
-#First get total posts
-url = info_address
-req = urllib2.Request(url)
-f = urllib2.urlopen(req)
-response = f.read()
-f.close()
-js = json.loads(response)
-total_posts = int(js['response']['blog']['posts'])
-print "Backing up " + js['response']['blog']['title'] + ", " + str(total_posts) + " posts."
+fout = None
 
-while offset < total_posts:
-	print str(offset) + "/" + str(total_posts)
-	url = posts_address + "&offset=" + str(offset)
-	req = urllib2.Request(url)
-	f = urllib2.urlopen(req)
-	response = f.read()
-	f.close()
-	#write to file
-	js = json.loads(response)
+info_address = "http://api.tumblr.com/v2/blog/"
+posts_address = "http://api.tumblr.com/v2/blog/"
+blog_name = "Unknown"
+blog_total_posts = 0
+backup_offset = 0 
+
+def loadSettings():
+	"""Loads script settings from a json file into memory. Exits on invalid JSON in file
+
+	    Raises:
+	        IOError: if the file does not exist
+	"""	
+	global settings
+	global fout
+	json_data=open(config_file)
+	try:
+		settings = json.load(json_data)
+	except ValueError as e:
+		print "The configuration file " + config_file + " exists but is not valid JSON. Either correct the error or revert to a working veersion"
+		print "Error details: " + str(e)
+		sys.exit(1)
+	fout = open(settings['backup_name'],'w')
+
+def getBlogInfo():
+	global blog_total_posts
+	global blog_title
+	global info_address
+	data = {"oauth_token":settings['oauth_token']}
+	consumer = oauth.Consumer(settings['consumer_key'], settings['consumer_secret'])
+	token = oauth.Token(settings['oauth_token'], settings['oauth_token_secret'])
+	client = oauth.Client(consumer,token)
+	resp, content = client.request(info_address, "POST", urllib.urlencode(data))
+	js = json.loads(content)
+	blog_total_posts = int(js['response']['blog']['posts'])
+	blog_title = js['response']['blog']['title']
+	
+def getTwentyPosts():
+	global settings
+	global backup_offset
+	global posts_address
+	data = {"offset":backup_offset,"oauth_token":settings['oauth_token']}
+	consumer = oauth.Consumer(settings['consumer_key'], settings['consumer_secret'])
+	token = oauth.Token(settings['oauth_token'], settings['oauth_token_secret'])
+	client = oauth.Client(consumer,token)
+	resp, content = client.request(posts_address, "POST", urllib.urlencode(data))
+	js = json.loads(content)
+ 	if js['meta']['status'] != 201 and js['meta']['status'] != 200: #something wrong
+		print js['meta']['msg']
+		print js
 	for i in js['response']['posts']:
 		out = json.dumps(i)
 		fout.write(out+'\n')
-	offset += 20
-fout.close()
+	backup_offset += 20
+
+if __name__ == '__main__':
+	loadSettings()
+	info_address  = info_address  + settings['blog'] + "/info?api_key="+settings['consumer_key']
+	posts_address = posts_address + settings['blog'] + "/posts?api_key="+settings['consumer_key']
+	if "oauth_token" not in settings.keys():
+		authorize()
+	getBlogInfo()
+	print "Backing up " + blog_title + ", " + str(blog_total_posts) + " posts."
+	while backup_offset < blog_total_posts:
+		getTwentyPosts()
+	print "Backup complete."
+	fout.close()
+
